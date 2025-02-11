d19f0e3802ce80dfc51eb5830aded5ad4aa985c8
Faran Humayun <faranhumayun@gmail.com>
2025-02-11 22:01:31 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <>
+      homepage
+      <div className="font-averia-serif-libre flex flex-col flex-wrap text-primary text-sm-custom">
+        <span>a</span>
+        <span>b</span>
+        <span>c</span>
+      </div>
+    </>
+  );
+}
+
+export default App;
