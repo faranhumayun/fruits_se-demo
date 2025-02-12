b7b53bc9be4ffe22b39933ecb62b2ea26ce950ec
Faran Humayun <faranhumayun@gmail.com>
2025-02-12 23:44:43 +0500

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
+import Navbar from "./components/Navbar/Navbar";
+
 function App() {
   return (
     <>
-      homepage
-      <div className="font-averia-serif-libre flex flex-col flex-wrap text-primary text-sm-custom">
-        <span>a</span>
-        <span>b</span>
-        <span>c</span>
-      </div>
+      <main className="overflow-x-hidden">
+        <Navbar />
+      </main>
     </>
   );
 }
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="container mx-auto">
+        <div>
+            <p>Fruit</p>
+            <p>Store</p>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
