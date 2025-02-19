b27ece6bf9f7b84b84b071035463356fbf36d13d
Faran Humayun <faranhumayun@gmail.com>
2025-02-19 18:20:03 +0500

    navbar changes

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -12,6 +12,39 @@ const Hero = () => {
                 <button className='text-white bg-primary px-4 py-2 rounded-xl'>Order Now</button>
             </div>
         </div>
+        <div className="container mx-auto">
+            <div>
+                <h1>Healthy</h1>
+                <br />
+                <h1>Fresh Fruits</h1>
+                <p className='capitalize'>order now for fresh healthy life</p>
+                <p className='capitalize'>Healthy and yummy food for fresh morning breakfast. Eat daily for good health and mind</p>
+                <p className='capitalize'>Order now and get 20% off on your first order</p>
+                <button className='text-white bg-primary px-4 py-2 rounded-xl'>Order Now</button>
+            </div>
+        </div>
+        <div className="container mx-auto">
+            <div>
+                <h1>Healthy</h1>
+                <br />
+                <h1>Fresh Fruits</h1>
+                <p className='capitalize'>order now for fresh healthy life</p>
+                <p className='capitalize'>Healthy and yummy food for fresh morning breakfast. Eat daily for good health and mind</p>
+                <p className='capitalize'>Order now and get 20% off on your first order</p>
+                <button className='text-white bg-primary px-4 py-2 rounded-xl'>Order Now</button>
+            </div>
+        </div>
+        <div className="container mx-auto">
+            <div>
+                <h1>Healthy</h1>
+                <br />
+                <h1>Fresh Fruits</h1>
+                <p className='capitalize'>order now for fresh healthy life</p>
+                <p className='capitalize'>Healthy and yummy food for fresh morning breakfast. Eat daily for good health and mind</p>
+                <p className='capitalize'>Order now and get 20% off on your first order</p>
+                <button className='text-white bg-primary px-4 py-2 rounded-xl'>Order Now</button>
+            </div>
+        </div>
     </section>
   )
 }
